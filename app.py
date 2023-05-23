fd41ee57988ae85e5b7cc6144acf4d38bc9426d2
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-05-23 22:47:31 +0200

    Update app.py
    change model

--- app.py
+++ app.py
@@ -52,7 +52,10 @@ def predict():
     { "prediction": [ 20.35373177134412 ] }
     """
     try:
-        clf = joblib.load("boston_housing_predict.joblib") 
+        # Load pretrained model as clf. Try any one model. 
+        #clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+        clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
+        #clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
