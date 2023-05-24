6ad22526076cbfee839db27a157a6edeef5186ea
Birte <78079178+BiBa-01@users.noreply.github.com>
2023-05-24 22:08:14 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,32 +1,32 @@
-from locust import HttpUser, task, between, TaskSet
+from locust import HttpUser, task, between
 
-class MyUser(HttpUser):
-    wait_time = between(5, 15)
+class WebsiteTestUser(HttpUser):
+    wait_time = between(0.5, 3.0)
 
-    @task
-    def index(self):
-        self.client.get("/")
+    @task(1)
+    def test_root(self):
+        self.client.get("")
 
-    @task
-    def load_predict(self):
+    @task(2)
+    def test_predict(self):
         self.client.post("predict", json={
-    "CHAS":{
-      "0":0
-   },
-   "RM":{
-      "0":6.575
-   },
-   "TAX":{
-      "0":296.0
-   },
-   "PTRATIO":{
-      "0":15.3
-   },
-   "B":{
-      "0":396.9
-   },
-   "LSTAT":{
-      "0":4.98
-   }        
-})    
-
+                                "CHAS":{
+                                    "0":0
+                                },
+                                "RM":{
+                                    "0":6.575
+                                },
+                                "TAX":{
+                                    "0":296.0
+                                },
+                                "PTRATIO":{
+                                    "0":15.3
+                                },
+                                "B":{
+                                    "0":396.9
+                                },
+                                "LSTAT":{
+                                    "0":4.98
+                                }
+                            }
+                        )
